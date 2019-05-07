5cda34f7b04a21cdd27b6630782cad8b495908f9
Andrea Boriani <andrea.boriani@logical.it>
2019-05-07 23:49:15 +0200

    First commit

--- Clipper_Test/Form1.vb
+++ Clipper_Test/Form1.vb
@@ -1,5 +1,17 @@
-﻿Public Class frmMain
+﻿Imports ClipperLib
+Public Class frmMain
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim subj As New List(Of List(Of IntPoint))
+        Dim clip As New List(Of List(Of IntPoint))
+        Dim solution As New PolyTree
+
+        Dim c As New Clipper()
+        c.AddPaths(subj, PolyType.ptSubject, True)
+        c.AddPaths(clip, PolyType.ptClip, True)
+        c.Execute(ClipType.ctIntersection, solution, PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd)
+        DrawPolygons(solution, Color.FromArgb(&H30, 0, &HFF, 0), Color.FromArgb(&HFF, 0, &H66, 0))
+    End Sub
+    Private Sub DrawPolygons(solution As PolyTree, c1 As Color, c2 As Color)
 
     End Sub
 End Class

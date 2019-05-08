af2fa38218644709682c9d45cdd368608e309d6e
andreab <andrea.boriani@logical.it>
2019-05-08 10:10:52 +0200

    Refactored code

--- Clipper_Test/Form1.Designer.vb
+++ Clipper_Test/Form1.Designer.vb
@@ -23,8 +23,27 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCw = New System.Windows.Forms.TextBox()
+        Me.txtCh = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSh = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtSw = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.trbX = New System.Windows.Forms.TrackBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.trbY = New System.Windows.Forms.TrackBar()
+        Me.txtCoffset = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtArea = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtPerimeter = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.trbX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.trbY, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
@@ -33,33 +52,218 @@ Partial Class frmMain
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.PictureBox1.Location = New System.Drawing.Point(193, 3)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(639, 501)
+        Me.PictureBox1.Size = New System.Drawing.Size(639, 530)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'Button1
+        'Label1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 455)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(160, 26)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 120)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Column width"
+        '
+        'txtCw
+        '
+        Me.txtCw.Location = New System.Drawing.Point(12, 136)
+        Me.txtCw.Name = "txtCw"
+        Me.txtCw.Size = New System.Drawing.Size(100, 20)
+        Me.txtCw.TabIndex = 3
+        Me.txtCw.Text = "30"
+        '
+        'txtCh
+        '
+        Me.txtCh.Location = New System.Drawing.Point(12, 189)
+        Me.txtCh.Name = "txtCh"
+        Me.txtCh.Size = New System.Drawing.Size(100, 20)
+        Me.txtCh.TabIndex = 5
+        Me.txtCh.Text = "30"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 173)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Column height"
+        '
+        'txtSh
+        '
+        Me.txtSh.Location = New System.Drawing.Point(12, 76)
+        Me.txtSh.Name = "txtSh"
+        Me.txtSh.Size = New System.Drawing.Size(100, 20)
+        Me.txtSh.TabIndex = 9
+        Me.txtSh.Text = "200"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 60)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Slab height"
+        '
+        'txtSw
+        '
+        Me.txtSw.Location = New System.Drawing.Point(12, 23)
+        Me.txtSw.Name = "txtSw"
+        Me.txtSw.Size = New System.Drawing.Size(100, 20)
+        Me.txtSw.TabIndex = 7
+        Me.txtSw.Text = "200"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Slab width"
+        '
+        'trbX
+        '
+        Me.trbX.Location = New System.Drawing.Point(12, 308)
+        Me.trbX.Maximum = 100
+        Me.trbX.Minimum = -100
+        Me.trbX.Name = "trbX"
+        Me.trbX.Size = New System.Drawing.Size(175, 45)
+        Me.trbX.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 281)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Column x position"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 356)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(89, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Column y position"
+        '
+        'trbY
+        '
+        Me.trbY.Location = New System.Drawing.Point(12, 383)
+        Me.trbY.Maximum = 100
+        Me.trbY.Minimum = -100
+        Me.trbY.Name = "trbY"
+        Me.trbY.Size = New System.Drawing.Size(175, 45)
+        Me.trbY.TabIndex = 12
+        '
+        'txtCoffset
+        '
+        Me.txtCoffset.Location = New System.Drawing.Point(15, 240)
+        Me.txtCoffset.Name = "txtCoffset"
+        Me.txtCoffset.Size = New System.Drawing.Size(100, 20)
+        Me.txtCoffset.TabIndex = 15
+        Me.txtCoffset.Text = "50"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 224)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(71, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Column offset"
+        '
+        'txtArea
+        '
+        Me.txtArea.Location = New System.Drawing.Point(12, 447)
+        Me.txtArea.Name = "txtArea"
+        Me.txtArea.ReadOnly = True
+        Me.txtArea.Size = New System.Drawing.Size(160, 20)
+        Me.txtArea.TabIndex = 17
+        Me.txtArea.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(9, 431)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(76, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Punching area"
+        '
+        'txtPerimeter
+        '
+        Me.txtPerimeter.Location = New System.Drawing.Point(12, 498)
+        Me.txtPerimeter.Name = "txtPerimeter"
+        Me.txtPerimeter.ReadOnly = True
+        Me.txtPerimeter.Size = New System.Drawing.Size(160, 20)
+        Me.txtPerimeter.TabIndex = 19
+        Me.txtPerimeter.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(9, 482)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(98, 13)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Punching perimeter"
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(834, 504)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(834, 533)
+        Me.Controls.Add(Me.txtPerimeter)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtArea)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtCoffset)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.trbY)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.trbX)
+        Me.Controls.Add(Me.txtSh)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSw)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtCh)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtCw)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "frmMain"
         Me.Text = "Clipper Test"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.trbX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.trbY, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCw As TextBox
+    Friend WithEvents txtCh As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSh As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtSw As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents trbX As TrackBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents trbY As TrackBar
+    Friend WithEvents txtCoffset As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtArea As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtPerimeter As TextBox
+    Friend WithEvents Label9 As Label
 End Class
--- Clipper_Test/Form1.vb
+++ Clipper_Test/Form1.vb
@@ -4,91 +4,195 @@ Imports System.Drawing.Drawing2D
 Imports System.Drawing.Imaging
 
 Public Class frmMain
-    Private Function PolygonToPointFArray(pg As List(Of IntPoint), scale As Double) As PointF()
+    Dim myBitmap As Bitmap
+    Dim newgraphic As Graphics
+    Dim slab As New RectPoly(200, 200, New IntPoint(0, 0))
+    Dim column As New RectPoly(30, 30, New IntPoint(0, 0))
+    Dim zoom As Double = 1
+    Dim offset As Double = 20
+    Dim pathArea As Double
+    Dim pathPerimeter As Double
 
+    Private Class RectPoly
+        Private _w, _h As Double
+        Private _origin As IntPoint
+        Private _poly As New List(Of IntPoint)
+        ReadOnly Property Poly As List(Of IntPoint)
+            Get
+                Return _poly
+            End Get
+        End Property
+        Property width As Double
+            Get
+                Return _w
+            End Get
+            Set(value As Double)
+                _w = value
+                _poly = BuildRectangularPolygon(_w, _h, _origin)
+            End Set
+        End Property
+        Property height As Double
+            Get
+                Return _h
+            End Get
+            Set(value As Double)
+                _h = value
+                _poly = BuildRectangularPolygon(_w, _h, _origin)
+            End Set
+        End Property
+        Property origin As IntPoint
+            Get
+                Return _origin
+            End Get
+            Set(value As IntPoint)
+                _origin = value
+                _poly = BuildRectangularPolygon(_w, _h, _origin)
+            End Set
+        End Property
+
+        Public Sub New(w As Double, h As Double, origin As IntPoint)
+            _w = w
+            _h = h
+            _origin = origin
+            _poly = BuildRectangularPolygon(_w, _h, _origin)
+        End Sub
+        Private Function BuildRectangularPolygon(width As Double, height As Double, origin As IntPoint) As List(Of IntPoint)
+            Dim rp As New List(Of IntPoint)
+            Dim ox As Double = origin.X
+            Dim oy As Double = origin.Y
+            Dim hw As Double = width / 2
+            Dim hh As Double = height / 2
+            rp.Add(New IntPoint(ox - hw, oy - hh))
+            rp.Add(New IntPoint(ox - hw, oy + hh))
+            rp.Add(New IntPoint(ox + hw, oy + hh))
+            rp.Add(New IntPoint(ox + hw, oy - hh))
+            Return rp
+        End Function
+    End Class
+    Private Function PolygonToPointFArray(pg As List(Of IntPoint)) As PointF()
         Dim result(pg.Count - 1) As PointF
+        Dim pbw As Double = PictureBox1.ClientRectangle.Width / 2
+        Dim pbh As Double = PictureBox1.ClientRectangle.Height / 2
         For i As Integer = 0 To pg.Count - 1
-            result(i) = New PointF(pg(i).X / scale + PictureBox1.ClientRectangle.Width / 2, pg(i).Y / scale + PictureBox1.ClientRectangle.Height / 2)
+            result(i) = New PointF(pg(i).X * zoom + pbw, pg(i).Y * zoom + pbh)
         Next
         Return result
     End Function
-    Dim myBitmap As Bitmap
-    Dim newgraphic As Graphics
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    Private Sub Draw()
         myBitmap = New Bitmap(PictureBox1.ClientRectangle.Width, PictureBox1.ClientRectangle.Height, PixelFormat.Format32bppArgb)
         newgraphic = Graphics.FromImage(myBitmap)
-        Dim subj As New List(Of List(Of IntPoint))
-        Dim clip As New List(Of List(Of IntPoint))
-        Dim solution As New List(Of List(Of IntPoint))
-        Dim solution2 As New List(Of List(Of IntPoint))
-        Dim b1, h1 As Double
-        Dim o1 As IntPoint
-        Dim b2, h2 As Double
-        Dim o2 As IntPoint
-        Dim offset As Double = 20
-        Dim scale As Double = 1
-        b1 = 30
-        h1 = 30
-        b2 = 100
-        h2 = 100
-        o1 = New IntPoint(-20, -25)
-        o2 = New IntPoint(0, 0)
-        Dim rect1 As New List(Of IntPoint)
-        Dim rect2 As New List(Of IntPoint)
-        rect1.Add(New IntPoint(o1.X - b1 / 2, o1.Y - h1 / 2))
-        rect1.Add(New IntPoint(o1.X - b1 / 2, o1.Y + h1 / 2))
-        rect1.Add(New IntPoint(o1.X + b1 / 2, o1.Y + h1 / 2))
-        rect1.Add(New IntPoint(o1.X + b1 / 2, o1.Y - h1 / 2))
-        DrawPolygon(rect1, Color.FromArgb(&HFF, 0, &HFF, 0), Color.FromArgb(&HFF, 20, 20, 20), scale)
-        Dim co As New ClipperOffset()
-        co.AddPath(rect1, JoinType.jtRound, EndType.etClosedPolygon)
-        co.Execute(solution2, offset * scale)
-        DrawPolygon(solution2, Color.FromArgb(0, 0, &HFF, 0), Color.FromArgb(&HFF, 0, 0, 0), scale)
-        rect2.Add(New IntPoint(o2.X - b2 / 2, o2.Y - h2 / 2))
-        rect2.Add(New IntPoint(o2.X - b2 / 2, o2.Y + h2 / 2))
-        rect2.Add(New IntPoint(o2.X + b2 / 2, o2.Y + h2 / 2))
-        rect2.Add(New IntPoint(o2.X + b2 / 2, o2.Y - h2 / 2))
-        DrawPolygon(rect2, Color.FromArgb(0, 0, &HFF, 0), Color.FromArgb(&HFF, 20, 20, 20), scale)
-
-        Dim c As New Clipper()
-        c.AddPath(rect2, PolyType.ptSubject, True)
-        c.AddPaths(solution2, PolyType.ptClip, True)
-        c.Execute(ClipType.ctIntersection, solution, PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd)
-        DrawPolygon(solution, Color.FromArgb(&H30, 0, 0, &HFF), Color.FromArgb(&HFF, 0, &H66, 0), scale)
-    End Sub
-    Private Sub DrawPolygon(solution As Object, c1 As Color, c2 As Color, scale As Double)
-
-
-
-
-
-        Dim path As New GraphicsPath()
-
         newgraphic.SmoothingMode = SmoothingMode.AntiAlias
+        newgraphic.Clear(Color.White)
+        Dim punchingPath As New List(Of List(Of IntPoint))
+        Dim offsetPolygon As New List(Of List(Of IntPoint))
+        DrawPolygon(slab.Poly, Color.Gray, Color.FromArgb(&HFF, 20, 20, 20))
+        DrawPolygon(column.Poly, Color.DarkGray, Color.FromArgb(&HFF, 20, 20, 20))
+        'do the offset
+        Dim co As New ClipperOffset()
+        co.AddPath(column.Poly, JoinType.jtRound, EndType.etClosedPolygon)
+        co.Execute(offsetPolygon, offset * zoom)
+        DrawPolygon(offsetPolygon, Color.FromArgb(0, 0, &HFF, 0), Color.FromArgb(&HFF, 0, 0, 0))
+        Dim c As New Clipper()
+        c.AddPath(slab.Poly, PolyType.ptSubject, True)
+        c.AddPaths(offsetPolygon, PolyType.ptClip, True)
+        c.Execute(ClipType.ctIntersection, punchingPath, PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd)
+        DrawPolygon(punchingPath, Color.FromArgb(&H30, 0, 0, &HFF), Color.FromArgb(&HFF, 0, &H66, 0))
+        pathArea = 0
+        pathPerimeter = 0
+        For Each p As List(Of IntPoint) In punchingPath
+            pathArea = pathArea + ClipperLib.Clipper.Area(p)
+            pathPerimeter = pathPerimeter + polygonPerimeter(p)
+        Next
+        txtArea.Text = Format(pathArea, "0.00")
+        txtPerimeter.Text = Format(pathPerimeter, "0.00")
+    End Sub
 
+    Private Function polygonPerimeter(polygon As List(Of IntPoint)) As Double
+        Dim p As Double
+        polygon.Add(polygon(0))
+        For i As Integer = 1 To polygon.Count - 1
+            p = p + Math.Sqrt((polygon(i).X - polygon(i - 1).X) ^ 2 + (polygon(i).Y - polygon(i - 1).Y) ^ 2)
+        Next
+        polygon.RemoveAt(polygon.Count - 1)
+        Return p
+    End Function
+    Private Function BuildRectangularPolygon(width As Double, height As Double, origin As IntPoint) As List(Of IntPoint)
+        Dim rp As New List(Of IntPoint)
+        Dim ox As Double = origin.X
+        Dim oy As Double = origin.Y
+        Dim hw As Double = width / 2
+        Dim hh As Double = height / 2
+        rp.Add(New IntPoint(ox - hw, oy - hh))
+        rp.Add(New IntPoint(ox - hw, oy + hh))
+        rp.Add(New IntPoint(ox + hw, oy + hh))
+        rp.Add(New IntPoint(ox + hw, oy - hh))
+        Return rp
+    End Function
+    Private Sub DrawPolygon(solution As Object, brushColor As Color, penColor As Color)
+        Dim path As New GraphicsPath()
 
         path.FillMode = FillMode.Winding
         If TypeOf solution Is List(Of List(Of IntPoint)) Then
             For Each pg As List(Of IntPoint) In DirectCast(solution, List(Of List(Of IntPoint)))
-                Dim pts As PointF() = PolygonToPointFArray(pg, scale)
+                Dim pts As PointF() = PolygonToPointFArray(pg)
                 path.AddPolygon(pts)
             Next
-
         ElseIf TypeOf solution Is List(Of IntPoint) Then
             Dim pg As List(Of IntPoint) = DirectCast(solution, List(Of IntPoint))
-            Dim pts As PointF() = PolygonToPointFArray(pg, scale)
+            Dim pts As PointF() = PolygonToPointFArray(pg)
             path.AddPolygon(pts)
-
-
         End If
-        Dim myBrush As New SolidBrush(c1)
-        Dim myPen As New Pen(c2)
+        Dim myBrush As New SolidBrush(brushColor)
+        Dim myPen As New Pen(penColor)
         myPen.Width = 1.0
-        'If myBrush.Color.A > 0 Then
         newgraphic.FillPath(myBrush, path)
-        'End If
         newgraphic.DrawPath(myPen, path)
         PictureBox1.Image = myBitmap
 
+    End Sub
+
+    Private Sub frmMain_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        On Error Resume Next
+        trbX.Minimum = -txtSw.Text / 2 + column.width / 2
+        trbX.Maximum = txtSw.Text / 2 - column.width / 2
+        trbY.Minimum = -txtSh.Text / 2 + column.height / 2
+        trbY.Maximum = txtSh.Text / 2 - column.height / 2
+        column.origin = New IntPoint(trbX.Value, trbY.Value)
+        Draw()
+    End Sub
+
+    Private Sub trb_ValueChanged(sender As Object, e As EventArgs) Handles trbX.ValueChanged, trbY.ValueChanged
+        column.origin = New IntPoint(trbX.Value, trbY.Value)
+        Draw()
+    End Sub
+
+    Private Sub txtCw_TextChanged(sender As Object, e As EventArgs) Handles txtCw.TextChanged
+        On Error Resume Next
+        column.width = txtCw.Text
+        trbX.Minimum = -txtSw.Text / 2 + column.width / 2
+        trbX.Maximum = txtSw.Text / 2 - column.width / 2
+        Draw()
+    End Sub
+    Private Sub txtCh_TextChanged(sender As Object, e As EventArgs) Handles txtCh.TextChanged
+        On Error Resume Next
+        column.height = txtCh.Text
+        trbY.Minimum = -txtSh.Text / 2 + column.height / 2
+        trbY.Maximum = txtSh.Text / 2 - column.height / 2
+        Draw()
+    End Sub
+    Private Sub txtSw_TextChanged(sender As Object, e As EventArgs) Handles txtSw.TextChanged
+        On Error Resume Next
+        slab.width = txtSw.Text
+        trbX.Minimum = -txtSw.Text / 2 + column.width / 2
+        trbX.Maximum = txtSw.Text / 2 - column.width / 2
+        Draw()
+    End Sub
+    Private Sub txtSh_TextChanged(sender As Object, e As EventArgs) Handles txtSh.TextChanged
+        On Error Resume Next
+        slab.height = txtSh.Text
+        trbY.Minimum = -txtSh.Text / 2 + column.height / 2
+        trbY.Maximum = txtSh.Text / 2 - column.height / 2
+        Draw()
     End Sub
 End Class
